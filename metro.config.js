93050e0e5528c1fd796183ebfb707fb5a142cf15
sangheon4353 <55776728+sangheon4353@users.noreply.github.com>
2026-04-27 22:28:45 +0900

    Update metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -3,9 +3,14 @@ const { withNativeWind } = require("nativewind/metro");
 
 const config = getDefaultConfig(__dirname);
 
+// Metro가 node_modules 내부의 캐시 파일을 추적할 수 있도록 경로 명시
+config.resolver.nodeModulesPaths = [
+  ...config.resolver.nodeModulesPaths,
+  'node_modules'
+];
+
 module.exports = withNativeWind(config, {
   input: "./global.css",
-  // Force write CSS to file system instead of virtual modules
-  // This fixes iOS styling issues in development mode
-  forceWriteFileSystem: true,
+  // Vercel(CI환경)에서는 파일 시스템 쓰기를 비활성화하거나 환경에 따라 조절
+  forceWriteFileSystem: process.env.NODE_ENV !== 'production', 
 });
